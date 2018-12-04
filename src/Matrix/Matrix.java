052ebf3797c34f9d7f56e0d4110a4bd4747c8434
stamie <estampel@gmail.com>
2018-12-04 16:53:22 +0100

    modification Matrix class

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -6,12 +6,59 @@
 package Matrix;
 
 import Matrix.Row;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
  * @author stampel
  */
 public class Matrix {
+    
+    protected List<Row> Rows;
+    protected long MoreClaimsCount;
+    
+    
+    Matrix() {
+        
+        this.Rows = new LinkedList();
+        this.MoreClaimsCount = 0;
+    
+    }
+    
+    Matrix(List<Row> rows) {
+        
+        this.Rows = new LinkedList();
+        this.Rows = rows;
+        this.setMoreClaimsCount();
+        
+    }
+    
+    public void setMoreClaimsCount() {
+    
+        for (Row row: this.Rows){
+            this.MoreClaimsCount+= row.getMoreClaimsCount();
+        
+        }
+    }
+    
+    public void addRow(Row row) {
+        
+        int i = 0;
+        for (Row r: this.Rows) {
+            if (r.getRowNum() == row.getRowNum()) {
+                return;
+            }
+            if (r.getRowNum() < row.getRowNum()) {
+                
+                this.Rows.add(i, r);
+                return;
+            }
+            i++;
+        
+        }
+    
+    }
     
     
 }
