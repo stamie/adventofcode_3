58435dd2e7708e072516d90bdc2e81719a6c8a81
stamie <estampel@gmail.com>
2018-12-04 19:41:34 +0100

    refactor

---
+++ src/adventofcode_3/Adventofcode_3.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adventofcode_3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.ToLongFunction;
+import java.util.stream.Stream;
+import java.lang.*;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+/**
+ *
+ * @author stampel
+ */
+public class Adventofcode_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String fileName = "/home/stampel/adventofcode_3/src/advent_proba.txt";
+       
+		//read file into stream, try-with-resources
+		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+                    List<String> y =  new ArrayList();
+                    stream.forEach(s-> {
+                    y.add(s);
+                    //System.out.println(s);
+                    });
+                    
+                    
+                    
+                    
+                    
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+}
